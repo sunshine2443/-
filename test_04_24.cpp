9e92d3700bad1f970ab90e8bcbbb3bcb30002b8f
sunshine2443 <98533988+sunshine2443@users.noreply.github.com>
2022-04-25 17:43:23 +0800

    Update test_04_24.cpp

--- test_04_24.cpp
+++ test_04_24.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int main()
+int main()\\判断由1,、2、3、4四个数字组成不相同无重复的数字的三位数并输出
 {
 	int i, j, k,n=0;
 	for (i = 1; i < 5; i++)
@@ -10,7 +10,7 @@ int main()
 					printf("%d%d%d\t", i, j, k);
 					if (++n % 5 == 0)printf("\n");
 				}
-	printf("\n¹²ÓÐ£º%d\n", n);
+	printf("\n共有%d\n", n);
 	return 0;
 }
 
